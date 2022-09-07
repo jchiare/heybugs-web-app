bac80ab99f96c98ce337e37af5e7f499df20f061
Jay Chiarella <jay.gch93@gmail.com>
2022-09-07 21:40:35 +0200

    fix optional env vars

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,11 +7,11 @@ import { z } from 'zod';
  */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(['development', 'test', 'production']),
-  NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  NODE_ENV: z.enum(['development', 'test', 'production']).optional(),
+  NEXTAUTH_SECRET: z.string().optional(),
+  NEXTAUTH_URL: z.string().url().optional(),
+  DISCORD_CLIENT_ID: z.string().optional(),
+  DISCORD_CLIENT_SECRET: z.string().optional(),
 });
 
 /**
